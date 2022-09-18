5e3bf911797c80fa77183d670b63d5486dec6d00
Defu Lian <dove.ustc@gmail.com>
2022-09-18 11:32:14 +0800

    reformat he config file of models

--- fuxits/utils/utils.py
+++ fuxits/utils/utils.py
@@ -89,7 +89,7 @@ def get_model(model_name):
     conf.update(parser_yaml(fname))
     fname = os.path.join(dir, 'config', model_name+'.yml')
     if os.path.isfile(fname):
-        conf.update({'model':parser_yaml(fname)})
+        conf.update(parser_yaml(fname))
     return model_class, conf
 
 def find_model(package, model_name):
